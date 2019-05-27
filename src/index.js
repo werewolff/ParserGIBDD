a8d465b5ab56c263731c6c6dbb64aa2f48105275
arahmatulin@b2broker.net <arahmatulin@b2broker.net>
2019-05-27 15:45:19 +0300

    refactoring. use env varables

--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
 const cheerio = require('cheerio')
 const fs = require('fs')
-const path = '/tmp/data.html'
-fs.exists(path, function (exists) {
+const DATA_DIR = process.env.DATA_DIR || '/tmp/'
+const FILE_NAME = process.env.FILE_NAME || '/tmp/data'
+
+function parse() {
+  const $ = cheerio.load(fs.readFileSync(FILE_NAME, 'utf-8'), { decodeEntities: false })
+  let result = {
+    title: '',
+    data: {}
+  }
+  result.title = $('.b-crash-stat th').html().trim().substring(24)
+  $('.b-crash-stat tr').each(function (i, elem) {
+    if (i > 1)
+      result['data'][$(this).children().first().text()] = $(this).children().last().text()
+  })
+  let date = new Date()
+  date = date.toISOString()
+    .replace(/[T:]/g, '-')
+    .replace(/\..+/, '')
+  
+  fs.writeFileSync(DATA_DIR + `data-${date}.json`, JSON.stringify(result, null, '\t'))
+}
+
+fs.exists(FILE_NAME, function (exists) {
   if (exists) {
-    const $ = cheerio.load(fs.readFileSync(path, 'utf-8'), { decodeEntities: false })
-    let result = {
-      title: '',
-      data: {}
-    }
-    result.title = $('.b-crash-stat th').html().trim().substring(24)
-    $('.b-crash-stat tr').each(function (i, elem) {
-      if (i > 1)
-        result['data'][$(this).children().first().text()] = $(this).children().last().text()
-    })
-    let date = new Date()
-    date = date.toISOString()
-      .replace(/[T:]/g, '-')
-      .replace(/\..+/, '')
-    fs.writeFileSync(`data-${date}.json`, JSON.stringify(result, null, '\t'))
-  } else
-    console.log(`File ${path} could't found`)
+    parse();
+  } else {
+    console.log(`File ${FILE_NAME} does not exists`)
+  }
 })
 
